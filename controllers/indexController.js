85f8ae3c9e4c6798d91e801e13d927dc300e7cc5
Soukorng <vongsoukorng34@gmail.com>
2024-12-29 11:20:15 +0700

    update

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const Movie = require("../models/movieModel");
 exports.getListProduct = async (req,res) => {
     try{
         const movies = await Movie.getAll();
-        title = "List movies"
+        const title = "List movies";
         res.render('movie/index',{movies,title});
     
     }catch(err){
